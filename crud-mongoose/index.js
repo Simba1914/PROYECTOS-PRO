70720254b96fe7229866a133aa877a755bd55233
Simba1914 <134667016+Simba1914@users.noreply.github.com>
2023-09-05 21:44:35 -0500

    crud-mongoose

--- crud-mongoose/index.js
+++ crud-mongoose/index.js
@@ -1,19 +1,22 @@
-import express from 'express';
+import express, { Router } from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
 console.log('Este es el servidor,👇👇👇👇 conectate yaaaaaaa  ');
 
 const server = express();
+const router = Router();
 const PORT = 3000;
+const URL = "mongodb+srv://luisH:191406@proyecto-db.inkcl1n.mongodb.net/social"
 
 
 server.use(express.json()); /*transforma el cuerpo de la peticion en json*/
 server.use(cors()); /*me permite recibir solicitudesw de clientes fuera de mi dominio*/
+server.use('/api/v1', router);
 
 
 async function main() {
-    await mongoose.connect("mongodb+srv://luisH:191406@proyecto-db.inkcl1n.mongodb.net/social");
+    await mongoose.connect(URL);
 
     // schema
 
@@ -26,6 +29,28 @@ async function main() {
 
     //USER MODEL
     const user = mongoose.model('User', userSchema );
+
+    //ROUTES
+    //CRUD
+    //get all users
+
+    /*http:localhost:3000/api/v1/users*/
+    router.get('/users',  async (request, response) =>{ 
+        // find all documents
+        const users = await user.find({});
+
+        response.status(200).json(users);
+    }); 
+
+    //get user by id
+    router.get('/users/:id', async (request, response) => {
+        const id = request.params.id;
+        const userFound = await user.find({_id:id}).exec();
+
+        console.log({userFound});
+
+    });
+    
 
     server.listen(PORT, () => {
         console.log(`server run in http://localhost:${PORT}`);
